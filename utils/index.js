2e2b2f92f6e5cccccfb9fa54ef1b3ed7e1d625b4
药小飒 <18895627215@163.com>
2021-11-05 10:05:33 +0800

    Create index.js

---
+++ utils/index.js
@@ -0,0 +1,27 @@
+数字
+
+1.生成指定范围随机数  
+
+export const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+2.数字千分位分割
+
+export const format = (n) => {
+    let num = n.toString();
+    let len = num.length;
+    if (len <= 3) {
+        return num;
+    } else {
+        let temp = '';
+        let remainder = len % 3;
+        if (remainder > 0) { // 不是3的整数倍
+            return num.slice(0, remainder) + ',' + num.slice(remainder, len).match(/\d{3}/g).join(',') + temp;
+        } else { // 3的整数倍
+            return num.slice(0, len).match(/\d{3}/g).join(',') + temp; 
+        }
+    }
+}
+
+
+
+
